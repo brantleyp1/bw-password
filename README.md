6622108fffd582c0f1ef6ecbaf4a097ee57706ef
Brantley Padgett <brantley.padgett@realpage.com>
2021-10-01 13:33:23 -0500

    updated requirements and test option

--- README.md
+++ README.md
@@ -9,16 +9,20 @@ With this I can set all the passwords at once and keep them in bitwarden and out
 
 ## Usage
 
-`./bwpass.sh` - will prompt you for a password. Note, there's no minimum password requirements, so just be honest with yourself.
+`./bwpass.sh` - will prompt you for a password.
 
 `./bwpass.sh 'someTH!ngH@rd'` - set a password to someTH!ngH@rd, just know that it's in your shell history if you're on a shared system.
 
+`./bwpass.sh -t 'someTH!ngH@rd'` - will test changing the password but will not make any changes.
+
 ## Requirements
 
-bitwarden-cli - required
-
-bitwarden account - this doesn't have to be self-hosted, though personally I think that's the best option ;-)
+|requirement| purpose | install via |
+|-----|-----|----|
+| jq | json query to parse the output of bw cli | `brew install jq` for macOS <br/> `apt/yum install jq` for Linux| 
+| bitwarden-cli | required  | `brew install bitwarden-cli` for macOS <br/> `snap install bw` for Linux running snap <br/> see [bitwarden.com](https://bitwarden.com/help/article/cli/) for more options |
+| bitwarden account | this doesn't have to be self-hosted, though personally I think that's the best option ;-) | |
 
 ## Notes
 
-There's a "test" function of sorts. Uncomment the first line in the for loop which will show you in a pretty output your new password.
+Updated to provide actual test functionality as well as password complexity. It is currently set at a minimum of 8 characters and verifies at least 1 uppper, 1 lower, 1 number and 1 special. Those are all configurable.
--- bwpass.sh
+++ bwpass.sh
@@ -1,12 +1,53 @@
 #! /bin/bash
-##find a folder named change, find all the items in the folder, and set login.password="newpass" for each item:
+
+## the folder name to search for, change as needed
+search=change
+
+## leave this variable
+test=FALSE
+
+## set password complexity here, currently set at minimum of 8 characters, 1 upper, 1 lower, and 1 special character. 
+minlength=8
+minupper=1
+minlower=1
+minspecial=1
+minnumber=1
+passcomplexity () {
+	LEN=${#newpass}
+
+	## only checks for 8 characters, that's up to you
+	if [[ "$LEN" -lt "$minlength" ]]; then
+		echo "Password is smaller than $minlength characters"
+		echo "Requirements are $minlength characters, $minupper Upper, $minlower Lower, $minspecial Special and $minnumber Numbers"
+		exit 4
+	fi
+	if [[ "$(printf %s "$newpass" | tr -d "[:digit:]" | tr -d "[:punct:]" | tr -d "[:lower:]" | wc -c)" -lt "$minupper" ]]; then
+		echo "Password doesn't contain enough uppercase letters"
+		echo "Requirements are $minlength characters, $minupper Upper, $minlower Lower, $minspecial Special and $minnumber Numbers"
+		exit 4
+	fi
+	if [[ "$(printf %s "$newpass" | tr -d "[:digit:]" | tr -d "[:punct:]" | tr -d "[:upper:]" | wc -c)" -lt "$minlower" ]]; then
+		echo "Password doesn't contain enough lowercase letters"
+		echo "Requirements are $minlength characters, $minupper Upper, $minlower Lower, $minspecial Special and $minnumber Numbers"
+		exit 4
+	fi
+	if [[ "$(printf %s "$newpass" | tr -d "[:alnum:]" | wc -c)" -lt "$minspecial" ]]; then
+		echo "Password doesn't contain enough special characters"
+		echo "Requirements are $minlength characters, $minupper Upper, $minlower Lower, $minspecial Special and $minnumber Numbers"
+		exit 4
+	fi
+	if [[ "$(printf %s "$newpass" | tr -d "[:alpha:]" | tr -d "[:punct:]" | wc -c)" -lt "$minnumber" ]]; then
+		echo "Password doesn't contain enough numbers"
+		echo "Requirements are $minlength characters, $minupper Upper, $minlower Lower, $minspecial Special and $minnumber Numbers"
+		exit 4
+	fi
+}
 
 setnewpass () {
-	echo "You didn't supply a new password, for shame..."
-	echo -e "\nLet's set one now...\n\nPassword:"
-	read -s newpass1
+	echo -e "\nLet's set a new password now...\n\nPassword:"
+	read -rs newpass1
 	echo -e "\nConfirm Password:"
-	read -s newpass2
+	read -rs newpass2
 	if [[ "${newpass1}" == "${newpass2}" ]]; then
 		newpass="${newpass2}"
 	else
@@ -15,14 +56,37 @@ setnewpass () {
 	fi
 }
 
-newpass="$1"
-[ -z "$1" ] && setnewpass
-#newpass='yourNewP@ss'
-search=change
+if [[ "$1" == "-t" ]]; then
+	test=TRUE
+	if [ -z "$2" ]; then
+		setnewpass
+	else
+		newpass="${2}"
+	fi
+elif [[ "$1" == "-h" ]]; then
+	echo -e "Usage:
+  $0
+    -h			Show this help screen.
+    -t			Test a password before setting.
+    'someH@RDpassword'	Enter a password on the command line (insecure)
+    [Enter]		Enter to be prompted for a password."
+	exit 3
+elif [ -n "$1" ]; then
+	test=FALSE
+	newpass="${1}"
+else
+	[ -z "$1" ] && setnewpass
+fi
+passcomplexity
 folderid=$(bw list folders --search "$search" | jq '.[].id' | tr -d '"')
 listofitems=$(bw list items --folderid "$folderid" | jq '.[].id' | tr -d '"')
-for i in ${listofitems};
-	#do bw get item $i --pretty | jq '.login.password="'"$newpass"'"'; ### comment this out and uncomment the line below to actually change passwords
-	do bw get item $i --pretty | jq '.login.password="'"$newpass"'"' | bw encode | bw edit item $i; ### comment this out and uncomment the line above to test everything
-done
+if [[ "$test" == "TRUE" ]]; then
+	for i in ${listofitems};
+		do bw get item $i | jq '.login.password="'"$newpass"'"' | jq '. | {Item: .id, "Password Would Be": .login.password}' | sed 's/[]{},"[]//g';
+	done
+else
+	for i in ${listofitems};
+		do bw get item $i | jq '.login.password="'"$newpass"'"' | bw encode | bw edit item $i | jq '. | {"Item Updated": .id}' | sed 's/[]{},"[]//g';
+	done
+fi
 exit
